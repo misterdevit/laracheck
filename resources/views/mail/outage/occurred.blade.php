77571e3dfd39511b5a2652a6d89ed7516f15fa90
Andrea Pollastri <scrivi@andreapollastri.net>
2025-01-10 05:32:04 +0100

    Fix outage email to display the latest outage details

--- resources/views/mail/outage/occurred.blade.php
+++ resources/views/mail/outage/occurred.blade.php
@@ -5,10 +5,10 @@
 The site {{ $site->name }} is currently down.
 
 <x-mail::table>
-| <span style="font-size: 16px; color: #000;">Outage #{{ $outage->id }}</span> |
+| <span style="font-size: 16px; color: #000;">Outage #{{ $site->outages()->last()->id }}</span> |
 | :------------ |
 | <b>URL          </b><br>{{ $site->url }} |
-| <b>Occurred At  </b><br>{{ $outage->occurred_at }} ({{ config('app.timezone') }})|
+| <b>Occurred At  </b><br>{{ $site->outages()->last()->occurred_at }} ({{ config('app.timezone') }})|
 </x-mail::table>
 
 Check the dashboard for more details!
